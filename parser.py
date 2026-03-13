29a5aeee99a85ea541e3efec19be2e5ce167e1e9
jannaberkhout <jannaberkhout@alwaysbelearning.com>
2026-03-13 14:24:29 +0100

    create dataframe with recipes

--- parser.py
+++ parser.py
@@ -2,21 +2,23 @@ import bs4
 import os
 from recipe import Ingredient, Recipe
 import re
+import pandas as pd
 
 CSS_INGREDIENTS = "div.ingredienten li"
 CSS_INFO_PERSONEN = "div.informatie.detail li.personen"
 CSS_INFO_BEREIDINGSTIJD = "div.informatie.detail li.bereiding"
-CSS_INFO_GERECHT = "div.informatie.detail li.gerecht"
+CSS_INFO_INSTRUCTIES = "span[itemprop='recipeInstructions']"
+CSS_INFO_RECEPTCAT = "div.informatie.detail li.gerecht"
 CSS_INFO_CALORIEN = "div.informatie.detail li.calorieen"
-
 CSS_PROTEIN = 'td[itemprop="proteinContent"]'
 CSS_H1 = "h1"
 
+rows = []
 dir = 'recipes/'
 for file_name in os.listdir(dir):
     with open(dir + file_name, 'r') as f:
         content = f.read()
-    soup = bs4.BeautifulSoup(content)
+    soup = bs4.BeautifulSoup(content, features="html.parser")
 
     name = soup.select_one(CSS_H1)
     if not name:
@@ -25,14 +27,28 @@ for file_name in os.listdir(dir):
     cal_str = soup.select_one(CSS_INFO_CALORIEN).text
     calories = int(re.findall(r'\d+', cal_str)[0])
     protein = int(re.findall(r'\d+', soup.select_one(CSS_PROTEIN).text)[0])
+    n_persons = int(re.findall(r'\d+', soup.select_one(CSS_INFO_PERSONEN).text)[0])
+    preptime = soup.select_one(CSS_INFO_BEREIDINGSTIJD).text
+    instructions = soup.select_one(CSS_INFO_INSTRUCTIES).text
+    receptcat = soup.select_one(CSS_INFO_RECEPTCAT).text
+    ingredients = [ingredient.text for ingredient in soup.select(CSS_INGREDIENTS)]
 
-    ingredients = []
-    ingredient_elements = soup.select(CSS_INGREDIENTS)
-    if not ingredient_elements:
-        continue
+    #rec = Recipe(name=name, n_persons= n_persons, preptime= preptime, ingredients=ingredients, instructions = instructions, protein=protein, calories=calories, receptcat= receptcat) 
+    #print(rec)
+    
 
-    for i in ingredient_elements:
-        ingredients.append(Ingredient(name=i.text))
+# voeg rij toe voor DataFrame
+    rows.append({
+        "name": name,
+        "ingredients": ingredients,     # string zodat df het netjes weergeeft
+        "instructions": instructions,
+        "preptime": preptime,
+        "n_persons": n_persons,
+        "calories": calories,
+        "protein": protein
+    })
 
-    rec = Recipe(name=name, ingredients=ingredients, protein=protein, calories=calories) 
-    print(rec)
+# maak DataFrame in één keer
+df = pd.DataFrame(rows, columns=["name", "ingredients", "instructions", "preptime", "n_persons", "calories", "protein"])
+
+pd.to_pickle(df, "data/veggie_recipes_struct.pkl")
--- recipe.py
+++ recipe.py
@@ -7,13 +7,23 @@ class Ingredient:
 @dataclass
 class Recipe:
     name: str
-    ingredients: list[Ingredient]
+    ingredients: list[str]
     protein: float
     calories: float
+    n_persons: int
+    receptcat: str
+    preptime: str
+    instructions: str
 
     def __repr__(self):
         ratio = self.protein / self.calories if self.calories > 0 else 0
         
         return (f"Recipe(name='{self.name}', "
+                f"preptime= {self.preptime} "
+                f"persons(persons={self.n_persons}, "
                 f"ingredients={len(self.ingredients)}, "
+                f"instructions={len(self.instructions)}, "
+                f"proteins={self.protein}, "
+                f"calories={self.calories}, "
+                f"receptcat={self.receptcat}, "
                 f"P/C Ratio: {ratio:.2f})")
